6065cc85ce15488dbd166acadc1288eb571adce6
Tushar Mann <tusharmann29@gmail.com>
2018-04-23 00:15:55 +0530

    payment received fragment completed

--- app/src/main/java/org/altbeacon/beaconreference/Payment_received_fragment.java
+++ app/src/main/java/org/altbeacon/beaconreference/Payment_received_fragment.java
@@ -1,10 +1,14 @@
 package org.altbeacon.beaconreference;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import static android.content.Context.MODE_PRIVATE;
 
 
 public class Payment_received_fragment extends Fragment {
@@ -14,6 +18,16 @@ public class Payment_received_fragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view=inflater.inflate(R.layout.fragment_payment_received, container, false);
+        TextView textView=(TextView)view.findViewById(R.id.payment);
+
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("Details",MODE_PRIVATE);
+        String phone=sharedPreferences.getString("phone","NULL");
+        String plate=sharedPreferences.getString("numplate","NULL");
+
+        String result="Payment for \nCar number "+plate+" received from \nPhone <b>"+phone+"</b>";
+        textView.setText(result);
+
+
         return view;
     }
 
